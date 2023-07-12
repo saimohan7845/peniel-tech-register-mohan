5de96b26ce7c7f8acde88e4a32a1b5cb8c157920
saimohan7845 <91629385+saimohan7845@users.noreply.github.com>
2023-07-12 18:32:18 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "assets/theme";
+import Presentation from "layouts/pages/presentation";
+import routes from "routes";
+import Dashboard from "layouts/pages/landing-pages/dashboard";
+
+export default function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return <Route exact path={route.route} element={route.component} key={route.key} />;
+      }
+
+      return null;
+    });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        {getRoutes(routes)}
+        <Route path="/penieltech" element={<Presentation />} />
+        <Route path="*" element={<Navigate to="/penieltech" />} />
+        <Route path="/pages/dashboard" element={<Dashboard />} />
+      </Routes>
+    </ThemeProvider>
+  );
+}
